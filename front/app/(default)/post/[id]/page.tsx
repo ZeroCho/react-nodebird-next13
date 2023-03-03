f82a8bda02d7c28ecb5073a72c56912c6938f3e0
Yoon-Hae-Min <yunhatmi@naver.com>
2023-03-03 23:47:04 +0900

    feat: 게시글 페이지 UI 생성

---
+++ front/app/(default)/post/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Tweet from "@/components/common/Tweet";
+import React from "react";
+
+const PostPage = () => {
+  return (
+    <>
+      <Tweet />
+      <Tweet />
+    </>
+  );
+};
+
+export default PostPage;
