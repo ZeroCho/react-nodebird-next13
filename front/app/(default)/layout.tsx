7134cc14f6a13b848a5c130accb778b8e1eced91
Yoon-Hae-Min <yunhatmi@naver.com>
2023-03-04 15:06:28 +0900

    feat: react-query provider 추가

--- front/app/(default)/layout.tsx
+++ front/app/(default)/layout.tsx
@@ -4,6 +4,7 @@ import NavigationBox from "@/components/common/NavigationBox";
 import SearchInput from "@/components/common/SearchInput";
 import SignInForm from "@/components/common/SignInForm";
 import { colors, Container, Grid, Link } from "@mui/material";
+import Providers from "./providers";
 
 export default function DefaultLayout({
   children,
@@ -11,42 +12,44 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Container maxWidth="xl">
-      <Grid container>
-        <Grid item md={3} sm={12} xs={12}>
-          <NavigationBox />
+    <Providers>
+      <Container maxWidth="xl">
+        <Grid container>
+          <Grid item md={3} sm={12} xs={12}>
+            <NavigationBox />
+          </Grid>
+          <Grid
+            item
+            md={6}
+            sm={12}
+            xs={12}
+            justifyContent="center"
+            sx={{
+              borderRight: "solid",
+              borderLeft: "solid",
+              borderWidth: "0.1rem",
+              borderColor: colors.grey[100],
+              padding: "0.8rem",
+            }}
+          >
+            <SearchInput />
+            {children}
+          </Grid>
+          <Grid
+            item
+            md={3}
+            sm={12}
+            xs={12}
+            justifyContent="center"
+            sx={{ padding: "0.8rem" }}
+          >
+            <SignInForm />
+            <Link href="https://www.zerocho.com/" color={colors.blue[700]}>
+              Made by ZeroCho
+            </Link>
+          </Grid>
         </Grid>
-        <Grid
-          item
-          md={6}
-          sm={12}
-          xs={12}
-          justifyContent="center"
-          sx={{
-            borderRight: "solid",
-            borderLeft: "solid",
-            borderWidth: "0.1rem",
-            borderColor: colors.grey[100],
-            padding: "0.8rem",
-          }}
-        >
-          <SearchInput />
-          {children}
-        </Grid>
-        <Grid
-          item
-          md={3}
-          sm={12}
-          xs={12}
-          justifyContent="center"
-          sx={{ padding: "0.8rem" }}
-        >
-          <SignInForm />
-          <Link href="https://www.zerocho.com/" color={colors.blue[700]}>
-            Made by ZeroCho
-          </Link>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Providers>
   );
 }
