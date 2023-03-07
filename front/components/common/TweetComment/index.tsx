d99eb4bbb05d8add11ebb6ea11e5f6f819c5c9c6
Yoon-Hae-Min <yunhatmi@naver.com>
2023-03-07 21:48:09 +0900

    feat: 댓글 UI 변경

--- front/components/common/TweetComment/index.tsx
+++ front/components/common/TweetComment/index.tsx
@@ -1,33 +1,57 @@
 import Comment from "@/typings/comment";
 import {
   Avatar,
+  CardHeader,
   Divider,
+  IconButton,
   List,
   ListItem,
   ListItemAvatar,
+  ListItemButton,
   ListItemText,
+  Paper,
   Typography,
 } from "@mui/material";
-import { useQuery } from "@tanstack/react-query";
-import React, { FC } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import React, { FC, useState } from "react";
 
 interface Props {
   comment: Comment;
 }
 
 const TweetComment: FC<Props> = ({ comment }) => {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const toggleDropDown = () => {
+    setIsDropDownOpen((pre) => !pre);
+  };
   return (
-    <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>{comment.User.nickname && comment.User.nickname[0]}</Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary={comment.User.nickname}
-          secondary={comment.content}
-        />
-      </ListItem>
-    </List>
+    <CardHeader
+      avatar={
+        <Avatar>{comment.User.nickname && comment.User.nickname[0]}</Avatar>
+      }
+      action={
+        <>
+          <IconButton aria-label="settings" onClick={toggleDropDown}>
+            <MoreVertIcon />
+          </IconButton>
+          <Paper
+            sx={{
+              display: isDropDownOpen ? "block" : "none",
+              position: "absolute",
+            }}
+          >
+            <List disablePadding>
+              <ListItemButton>
+                <DeleteIcon />
+              </ListItemButton>
+            </List>
+          </Paper>
+        </>
+      }
+      title={comment.User.nickname}
+      subheader={comment.content}
+    />
   );
 };
 
