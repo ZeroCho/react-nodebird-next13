b7d7f9565480b52e8b9a643ea4e7b35bdf8b59c8
Yoon-Hae-Min <yunhatmi@naver.com>
2023-03-07 23:30:33 +0900

    feat: 팔로우 팔로잉 UI변경

--- front/app/(default)/profile/page.tsx
+++ front/app/(default)/profile/page.tsx
@@ -18,6 +18,7 @@ import React from "react";
 import FolderIcon from "@mui/icons-material/Folder";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditUserInfo from "@/components/profile/EditUserInfo";
+import FollowList from "@/components/profile/FollowList";
 
 const ProfilePage = () => {
   return (
@@ -32,54 +33,38 @@ const ProfilePage = () => {
       >
         <EditUserInfo />
       </Box>
-      <Box
-        component="div"
-        sx={{ mt: "1rem", backgroundColor: colors.grey[50], padding: "1rem" }}
-      >
-        <Typography variant="h6" sx={{ textAlign: "center", mb: "1rem" }}>
-          팔로잉
-        </Typography>
-        <List>
-          <ListItem
-            secondaryAction={
-              <IconButton edge="end" aria-label="delete">
-                <DeleteIcon />
-              </IconButton>
-            }
+      <Grid container spacing={2}>
+        <Grid item md={6}>
+          <Box
+            component="div"
+            sx={{
+              mt: "1rem",
+              backgroundColor: colors.grey[50],
+              padding: "1rem",
+            }}
           >
-            <ListItemAvatar>
-              <Avatar>
-                <FolderIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Single-line item" />
-          </ListItem>
-        </List>
-      </Box>
-      <Box
-        component="div"
-        sx={{ mt: "1rem", backgroundColor: colors.grey[50], padding: "1rem" }}
-      >
-        <Typography variant="h6" sx={{ textAlign: "center", mb: "1rem" }}>
-          팔로우
-        </Typography>
-        <List>
-          <ListItem
-            secondaryAction={
-              <IconButton edge="end" aria-label="delete">
-                <DeleteIcon />
-              </IconButton>
-            }
+            <Typography variant="h6" sx={{ textAlign: "center", mb: "1rem" }}>
+              팔로잉
+            </Typography>
+            <FollowList />
+          </Box>
+        </Grid>
+        <Grid item md={6}>
+          <Box
+            component="div"
+            sx={{
+              mt: "1rem",
+              backgroundColor: colors.grey[50],
+              padding: "1rem",
+            }}
           >
-            <ListItemAvatar>
-              <Avatar>
-                <FolderIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Single-line item" />
-          </ListItem>
-        </List>
-      </Box>
+            <Typography variant="h6" sx={{ textAlign: "center", mb: "1rem" }}>
+              팔로우
+            </Typography>
+            <FollowList />
+          </Box>
+        </Grid>
+      </Grid>
     </>
   );
 };
---
+++ front/components/profile/FollowList/index.tsx
@@ -0,0 +1,48 @@
+import {
+  Avatar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
+import FolderIcon from "@mui/icons-material/Folder";
+import DeleteIcon from "@mui/icons-material/Delete";
+import React from "react";
+
+const FollowList = () => {
+  return (
+    <List>
+      <ListItem
+        secondaryAction={
+          <IconButton edge="end" aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        }
+      >
+        <ListItemAvatar>
+          <Avatar>
+            <FolderIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Single-line item" />
+      </ListItem>
+      <ListItem
+        secondaryAction={
+          <IconButton edge="end" aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        }
+      >
+        <ListItemAvatar>
+          <Avatar>
+            <FolderIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Single-line item" />
+      </ListItem>
+    </List>
+  );
+};
+
+export default FollowList;
