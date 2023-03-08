f989e58eb01a98f7e17f4ba8f2751012d34a65e4
Yoon-Hae-Min <yunhatmi@naver.com>
2023-03-08 16:29:08 +0900

    feat: UserProfile navigation 기능 추가

--- front/app/(default)/user/[id]/page.tsx
+++ front/app/(default)/user/[id]/page.tsx
@@ -2,13 +2,7 @@ import TweetCard from "@/components/common/TweetCard";
 import React from "react";
 
 const UserTweetPage = () => {
-  return (
-    <>
-      <TweetCard />
-      <TweetCard />
-      <TweetCard />
-    </>
-  );
+  return <>유저페이지</>;
 };
 
 export default UserTweetPage;
