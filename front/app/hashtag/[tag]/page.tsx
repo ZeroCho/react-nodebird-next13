1cea5dd854b4544204f089ecd0275c00e6810eca
Yoon-Hae-Min <yunhatmi@naver.com>
2023-03-12 21:48:53 +0900

    feat: 해시태그 검색 결과 기능 추가

--- front/app/hashtag/[tag]/page.tsx
+++ front/app/hashtag/[tag]/page.tsx
@@ -1,13 +1,9 @@
 import { TweetCard } from "@/components/Tweets/TweetCard";
 import React from "react";
+import ClientPage from "./ClientPage";
 
-const HashTagPage = () => {
-  return (
-    <>
-      <TweetCard />
-      <TweetCard />
-    </>
-  );
+const HashTagPage = ({ params }: { params: { tag: string } }) => {
+  return <ClientPage params={params} />;
 };
 
 export default HashTagPage;
