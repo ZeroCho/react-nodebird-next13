16613211d6b4eb538b8c1778e5a6376793ee2a99
Yoon-Hae-Min <yunhatmi@naver.com>
2023-03-19 19:26:34 +0900

    feat: 신고 modal UI 생성

--- front/components/Tweets/TweetCard/TweetCardHeader/index.tsx
+++ front/components/Tweets/TweetCard/TweetCardHeader/index.tsx
@@ -9,6 +9,7 @@ import {
 } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import DeleteIcon from "@mui/icons-material/Delete";
+import ReportIcon from "@mui/icons-material/Report";
 import React, { FC, useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import useFollowMutation from "@/hooks/mutations/useFollowMutation";
@@ -18,6 +19,7 @@ import User from "@/typings/user";
 import { loadMyInfoAPI } from "@/apis/auth";
 import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
+import ReportModal from "../../ReportModal";
 
 interface Props {
   user: Partial<User> & { id: number };
@@ -28,7 +30,10 @@ interface Props {
 const TweetCardHeader: FC<Props> = ({ user, postId, createdAt }) => {
   const queryClient = useQueryClient();
   const router = useRouter();
+
   const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const [showReportModal, setShowReportModal] = useState(false);
+
   const { data: me } = useQuery<User>(["user"], loadMyInfoAPI);
   const isFollowing = me?.Followings?.find((v) => v.id === user.id);
 
@@ -47,49 +52,64 @@ const TweetCardHeader: FC<Props> = ({ user, postId, createdAt }) => {
     setIsDropDownOpen((pre) => !pre);
   };
 
+  const toggleReportModal = () => {
+    setShowReportModal((pre) => !pre);
+  };
+
   return (
-    <CardHeader
-      avatar={
-        <Avatar
-          sx={{ bgcolor: "red", cursor: "pointer" }}
-          aria-label="recipe"
-          onClick={() => router.push(`user/${user.id}`)}
-        >
-          {user.nickname && user.nickname[0]}
-        </Avatar>
-      }
-      action={
-        <>
-          <IconButton aria-label="settings" onClick={toggleDropDown}>
-            <MoreVertIcon />
-          </IconButton>
-          <Paper
-            sx={{
-              display: isDropDownOpen ? "block" : "none",
-              position: "absolute",
-            }}
+    <>
+      <ReportModal open={showReportModal} handleClose={toggleReportModal} />
+      <CardHeader
+        avatar={
+          <Avatar
+            sx={{ bgcolor: "red", cursor: "pointer" }}
+            aria-label="recipe"
+            onClick={() => router.push(`user/${user.id}`)}
           >
-            <List disablePadding>
-              <ListItemButton onClick={() => removePostMutate()}>
-                <DeleteIcon />
-              </ListItemButton>
-            </List>
-          </Paper>
-        </>
-      }
-      title={
-        <>
-          {user.nickname}
-          {me?.id !== user.id &&
-            (isFollowing ? (
-              <Button onClick={() => unFollowMutate(user.id)}>언팔로우</Button>
-            ) : (
-              <Button onClick={() => followMutate(user.id)}>팔로우</Button>
-            ))}
-        </>
-      }
-      subheader={dayjs(createdAt).format("YYYY.MM.DD")}
-    />
+            {user.nickname && user.nickname[0]}
+          </Avatar>
+        }
+        action={
+          <>
+            <IconButton aria-label="settings" onClick={toggleDropDown}>
+              <MoreVertIcon />
+            </IconButton>
+            <Paper
+              sx={{
+                display: isDropDownOpen ? "block" : "none",
+                position: "absolute",
+              }}
+            >
+              <List disablePadding>
+                {me?.id === user.id ? (
+                  <ListItemButton onClick={() => removePostMutate()}>
+                    <DeleteIcon />
+                  </ListItemButton>
+                ) : (
+                  <ListItemButton onClick={toggleReportModal}>
+                    <ReportIcon />
+                  </ListItemButton>
+                )}
+              </List>
+            </Paper>
+          </>
+        }
+        title={
+          <>
+            {user.nickname}
+            {me?.id !== user.id &&
+              (isFollowing ? (
+                <Button onClick={() => unFollowMutate(user.id)}>
+                  언팔로우
+                </Button>
+              ) : (
+                <Button onClick={() => followMutate(user.id)}>팔로우</Button>
+              ))}
+          </>
+        }
+        subheader={dayjs(createdAt).format("YYYY.MM.DD")}
+      />
+    </>
   );
 };
 
