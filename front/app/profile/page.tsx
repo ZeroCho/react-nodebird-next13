d952ddf6399b0babd8343a310fae1dde8e4c11b1
Yoon-Hae-Min <yunhatmi@naver.com>
2023-03-20 14:36:48 +0900

    feat: 로그인 하지 않으면 프로필 페이지에 접근 불가

--- front/app/profile/page.tsx
+++ front/app/profile/page.tsx
@@ -1,107 +1,19 @@
-"use client";
-
-import { Box, colors, Grid, Typography } from "@mui/material";
-import React from "react";
-import EditUserProfile from "@/components/Users/EditUserProfile";
-import FollowList from "@/components/Users/FollowList";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import User from "@/typings/user";
 import { loadMyInfoAPI } from "@/apis/auth";
-import { loadFollowersAPI, loadFollowingsAPI } from "@/apis/user";
-import { AxiosError } from "axios";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import React from "react";
+import ProfilePage from "./page.client";
 
-const ProfilePage = () => {
-  const {
-    data: followings,
-    isLoading: followingsLoading,
-    error: followingsError,
-    fetchNextPage: fetchNextFollowings,
-    hasNextPage: hasNextFollowings,
-  } = useInfiniteQuery<User[], AxiosError>(
-    ["followings"],
-    ({ pageParam = 5 }) => loadFollowingsAPI(pageParam),
-    {
-      getNextPageParam: (lastPage, pages) => {
-        if (lastPage.length < 3) return;
-        return pages.length;
-      },
-    }
-  );
-
-  const {
-    data: followers,
-    isLoading: followersLoading,
-    error: followersError,
-    fetchNextPage: fetchNextFollowers,
-    hasNextPage: hasNextFollowers,
-  } = useInfiniteQuery<User[], AxiosError>(
-    ["followers"],
-    ({ pageParam = 5 }) => loadFollowersAPI(pageParam),
-    {
-      getNextPageParam: (lastPage, pages) => {
-        if (lastPage.length < 3) return;
-        return pages.length;
-      },
-    }
-  );
-  return (
-    <>
-      <Box
-        component="div"
-        sx={{
-          mt: "1rem",
-          backgroundColor: colors.grey[50],
-          padding: "1rem",
-        }}
-      >
-        <EditUserProfile />
-      </Box>
-      <Grid container spacing={2}>
-        <Grid item md={6}>
-          <Box
-            component="div"
-            sx={{
-              mt: "1rem",
-              backgroundColor: colors.grey[50],
-              padding: "1rem",
-            }}
-          >
-            <Typography variant="h6" sx={{ textAlign: "center", mb: "1rem" }}>
-              팔로잉
-            </Typography>
-            {followings && (
-              <FollowList
-                data={followings}
-                fetchNextPage={fetchNextFollowings}
-                hasNextPage={hasNextFollowings}
-              />
-            )}
-          </Box>
-        </Grid>
-        <Grid item md={6}>
-          <Box
-            component="div"
-            sx={{
-              mt: "1rem",
-              backgroundColor: colors.grey[50],
-              padding: "1rem",
-            }}
-          >
-            <Typography variant="h6" sx={{ textAlign: "center", mb: "1rem" }}>
-              팔로우
-            </Typography>
-            {followers && (
-              <FollowList
-                data={followers}
-                fetchNextPage={fetchNextFollowers}
-                hasNextPage={hasNextFollowers}
-              />
-            )}
-          </Box>
-        </Grid>
-      </Grid>
-    </>
-  );
+const Page = async () => {
+  const header = headers();
+  const cookie = header.get("Cookie");
+  const data = await loadMyInfoAPI({
+    headers: cookie ? { cookie } : undefined,
+  });
+  if (!data) {
+    redirect("/");
+  }
+  return <ProfilePage />;
 };
 
-export default ProfilePage;
+export default Page;
